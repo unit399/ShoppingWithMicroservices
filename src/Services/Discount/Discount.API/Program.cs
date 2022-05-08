faa849da654fbef735551d8472eeb43b4c6cb1d8
Okan Çilingiroğlu <okan.cilingiroglu@gmail.com>
2022-05-08 14:08:53 +0300

    Discount.API and PostgreSql

---
+++ src/Services/Discount/Discount.API/Program.cs
@@ -0,0 +1,33 @@
+using Discount.API.Repositories;
+using Microsoft.OpenApi.Models;
+using Discount.API.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Discount.API", Version = "v1" });
+});
+
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Discount.API v1"));
+}
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MigrateDatabase<Program>();
+
+app.Run();
