0469a944fc7788b051cd2c9b6c8b1986b253da97
Okan Çilingiroğlu <okan.cilingiroglu@gmail.com>
2022-05-08 13:05:00 +0300

    Basket.API and Redis

---
+++ src/Services/basket/Basket.API/Program.cs
@@ -0,0 +1,35 @@
+using Basket.API.Repositories;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString");
+    Console.WriteLine(options.Configuration);
+});
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Basket.API", Version = "v1" });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Basket.API v1"));
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
